baec3e879f0fc3efcc82b4b67ab46f5def55fd14
Jesse <jesse.earley@gmail.com>
2022-03-21 15:00:37 -0400

    added comments, cleaned up code

--- Video-InfoBox/videoInfoBox.js
+++ Video-InfoBox/videoInfoBox.js
@@ -96,6 +96,7 @@ class VideoInfoBox extends HTMLElement{
             contentTitle = 'Heading'; //Need to set a default if one is not defined by the user
         }
 
+        //Check to see if user included a title
         let contentDescription = '';
         if (this.getElementsByTagName('p').length >= 1) {
             contentDescription = this.getElementsByTagName('p')[0].innerHTML; //If so, grab it.
@@ -104,25 +105,24 @@ class VideoInfoBox extends HTMLElement{
             contentDescription = 'Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.'; //Need to set a default if one is not defined by the user
         }
 
+        //Check to see if user included a link
         let contentLink = '';
         if (this.getElementsByTagName('a').length >= 1){
-            contentLink = this.getElementsByTagName('a')[0].href;
-            console.log(contentLink);
+            contentLink = this.getElementsByTagName('a')[0].href; //If so, grab it.
         }
         else{
-            contentLink = false;
+            contentLink = false; //If not, let's set it to false
         }
 
         this.populateContent(contentTitle, contentDescription, contentLink);
     }
 
-    //Method - Populate the description box with title, paragraph and link (if available).
+    //Method - Populate the description box with title, paragraph and link (if applicable).
     populateContent(contentTitle, contentDescription, contentLink){
         this.shadowRoot.querySelector('h2').innerText = contentTitle;
         this.shadowRoot.querySelector('p').innerText = contentDescription;
         if(contentLink !== false){
             let anchor = document.createElement('a');
-            console.log(anchor);
             anchor.innerText = 'View More';
             anchor.setAttribute('href', contentLink);
             this.shadowRoot.querySelector('.description').appendChild(anchor);
